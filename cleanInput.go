68fc14acd893a25f9a43cf0874fdd708c1df2eba
chonlaphoom <chonlaphoomth@gmail.com>
2025-04-03 21:32:18 +0700

    first test

---
+++ cleanInput.go
@@ -0,0 +1,22 @@
+package main
+
+func cleanInput(input string) []string {
+	result := []string{}
+
+	temp := ""
+	for i, r := range input {
+		if r == ' ' {
+			if temp != "" {
+				result = append(result, temp)
+				temp = ""
+			}
+		} else {
+			temp += string(r)
+		}
+
+		if l := i + 1; len(input) == l && temp != "" {
+			result = append(result, temp)
+		}
+	}
+	return result
+}
