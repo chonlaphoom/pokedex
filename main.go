734f081bd23c823db5b32ff31b49e51c81bd859c
chonlaphoom <chonlaphoomth@gmail.com>
2025-04-05 15:40:47 +0700

    add map, pmap command

--- main.go
+++ main.go
@@ -2,41 +2,61 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
+var globalState = State{
+	Current: "https://pokeapi.co/api/v2/location",
+}
+
+type State struct {
+	Current  string
+	Previous *string
+	Next     *string
+}
+
 type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
+	Name        string
+	Description string
+	Callback    func() error
 }
 
-type location struct {
-	name string
-	utl  string
+type Location struct {
+	Name string `json:"name"`
 }
 
-type baseResponse[T any] struct {
-	count    int
-	next     string
-	previous any
-	results  []T
+type BaseResponse[T any] struct {
+	Next     string `field:"next"`
+	Previous string `field:"previous"`
+	Results  []T    `field:"results"`
 }
 
 var generalRegistry = map[string]cliCommand{
 	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
+		Name:        "exit",
+		Description: "Exit the Pokedex",
+		Callback:    commandExit,
 	},
-	"map": {},
+	"map": {
+		Name:        "map",
+		Description: "Get the map of the Pokemon world",
+		Callback:    commandMap,
+	},
+	"pmap": {
+		Name:        "pmap",
+		Description: "Get the map of previois Pokemon world",
+		Callback:    commandPMap,
+	},
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-
 		fmt.Print("Pokedex > ")
 		var err error
 		if scanner.Scan() {
@@ -51,7 +71,10 @@ func main() {
 			}
 
 			if cmd, ok := generalRegistry[text]; ok {
-				err = cmd.callback()
+				err = cmd.Callback()
+				if err != nil {
+					log.Fatal(err)
+				}
 
 			} else if text == "help" {
 				err = commandHelp()
@@ -83,8 +106,73 @@ func commandHelp() error {
 
 	fmt.Println("help: Displays a help message")
 	for _, cmd := range generalRegistry {
-		text := fmt.Sprintf("%s: %s", cmd.name, cmd.description)
+		text := fmt.Sprintf("%s: %s", cmd.Name, cmd.Description)
 		fmt.Println(text)
+	}
+	return nil
+}
+
+func getUrl(isPrev bool) string {
+	if isPrev && globalState.Previous != "" {
+		return globalState.Previous
+	}
+
+	if globalState.Next != "" {
+		return globalState.Next
+	}
+
+	return globalState.Current
+}
+
+func fetchAndPrint(url string) error {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, readErr := io.ReadAll(resp.Body)
+	if readErr != nil {
+		return readErr
+	}
+
+	var baseResponse BaseResponse[Location] = BaseResponse[Location]{}
+
+	err = json.Unmarshal(body, &baseResponse)
+
+	if err != nil {
+		return err
+	}
+
+	locations := baseResponse.Results
+
+	for _, location := range locations {
+		fmt.Println(location.Name)
+	}
+
+	globalState.Previous = baseResponse.Previous
+	globalState.Next = baseResponse.Next
+
+	return nil
+}
+
+func commandMap() error {
+	url := getUrl(false)
+	err := fetchAndPrint(url)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func commandPMap() error {
+	url := getUrl(true)
+	err := fetchAndPrint(url)
+	if err != nil {
+		return err
 	}
 	return nil
 }
